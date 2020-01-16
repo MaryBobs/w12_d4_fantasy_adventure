e09ccb77296b1b0bdb0528b6d4fd42f41dd442d6
MaryBobs <55357115+MaryBobs@users.noreply.github.com>
2020-01-16 11:47:56 +0000

    creates IHealth

---
+++ src/main/java/behaviours/IHealth.java
@@ -0,0 +1,8 @@
+package behaviours;
+
+public interface IHealth {
+
+    int getHealth();
+    int setHealth();
+
+}
--- src/main/java/player/Player.java
+++ src/main/java/player/Player.java
@@ -1,12 +1,47 @@
 package player;
 
+import behaviours.IHealth;
 import skill.Skill;
+import valuables.Valuable;
 
-public abstract class Player {
+import java.util.ArrayList;
+
+public abstract class Player implements IHealth {
 
     private int health;
     private Skill skill;
     private ArrayList<Valuable> treasures;
+
+    public Player(int health, Skill skill) {
+        this.health = health;
+        this.skill = skill;
+        this.treasures = new ArrayList<Valuable>();
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public Skill getSkill() {
+        return skill;
+    }
+
+    public ArrayList<Valuable> getTreasures() {
+        return treasures;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public void setSkill(Skill skill) {
+        this.skill = skill;
+    }
+
+    public void setTreasures(ArrayList<Valuable> treasures) {
+        this.treasures = treasures;
+    }
+
 
 
 }
