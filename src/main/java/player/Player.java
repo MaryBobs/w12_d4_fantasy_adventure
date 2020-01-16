950c80a7ed44efc2beda1a0ec15c3c55534b0a82
MaryBobs <55357115+MaryBobs@users.noreply.github.com>
2020-01-16 11:43:20 +0000

    creates skill class

---
+++ src/main/java/player/Player.java
@@ -0,0 +1,11 @@
+package player;
+
+public abstract class Player {
+
+    private int health;
+    private Skill skill;
+    private ArrayList<Valuable> treasures;
+
+
+
+}
---
+++ src/main/java/skill/Skill.java
@@ -0,0 +1,20 @@
+package skill;
+
+public class Skill {
+
+    private String name;
+    private int value;
+
+    public Skill(String name, int value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
